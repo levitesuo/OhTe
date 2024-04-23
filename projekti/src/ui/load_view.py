5f4a123ea79f5f80ce45aeb9ae214fa10e4dee48
levitesuo <leevi.suotula@gmail.com>
2024-04-23 19:26:55 +0300

    Added load menu. Not 100% working yet, but the menu exists

--- projekti/src/ui/load_view.py
+++ projekti/src/ui/load_view.py
@@ -19,15 +19,28 @@ class LoadView:
 
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
+        big_lable = ttk.Label(master=self._frame,
+                              text="Load",
+                              font=("Noto Mono", 20, 'bold')
+                              )
+        big_lable.pack(pady=10)
         maps = [mi.get_grid() for mi in gol_service.get_boards()]
+        scroll_frame = cttk.CTkScrollableFrame(
+            master=self._frame,
+            width=400,
+            height=350)
+        scroll_frame.pack(pady=20)
         for map in maps:
-            sub_frame = ttk.Frame(master=self._frame)
+            sub_frame = ttk.Frame(master=scroll_frame)
             lable = ttk.Label(master=sub_frame, text=f"{map['name']}")
             lable.pack(side='left')
             button = ttk.Button(master=sub_frame, text='load',
                                 command=lambda: self._pygame_command(map['grid_id']))
             button.pack(side='right')
             sub_frame.pack(side='top')
+        cancel_button = ttk.Button(
+            master=self._frame, text='cancel', command=self._cancel_command)
+        cancel_button.pack()
 
     def pack(self):
         self._frame.pack()

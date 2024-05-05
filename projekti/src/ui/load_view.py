ffd602218fd0d48b06dcf695279aaa7c5a206ebc
levitesuo <leevi.suotula@gmail.com>
2024-05-05 14:07:50 +0300

    Last code changes made. Load view polished

--- projekti/src/ui/load_view.py
+++ projekti/src/ui/load_view.py
@@ -1,5 +1,7 @@
 from tkinter import ttk
+from tkinter import font
 import customtkinter as cttk
+from functools import partial
 from services import gol_service
 
 
@@ -21,7 +23,7 @@ class LoadView:
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
         big_lable = ttk.Label(master=self._frame,
-                              text="Load",
+                              text="Load map",
                               font=("Noto Mono", 20, 'bold')
                               )
         big_lable.pack(pady=10)
@@ -32,13 +34,20 @@ class LoadView:
             height=350)
         scroll_frame.pack(pady=20)
         for m in maps:
-            sub_frame = ttk.Frame(master=scroll_frame)
-            lable = ttk.Label(master=sub_frame, text=f"{m['name']}")
-            lable.pack(side='left')
-            button = ttk.Button(master=sub_frame, text='load',
-                                command=lambda: self._pygame_command(m['grid_id']))
+            sub_frame = cttk.CTkFrame(master=scroll_frame)
+            button = cttk.CTkButton(master=sub_frame,
+                                    text=m['name'],
+                                    corner_radius=2,
+                                    width=200,
+                                    fg_color='grey',
+                                    hover_color="#ebebeb",
+                                    text_color='black',
+                                    font=(font.nametofont(
+                                        "TkDefaultFont"), 15),
+                                    command=partial(
+                                        self._pygame_command, m['grid_id']))
             button.pack(side='right')
-            sub_frame.pack(side='top')
+            sub_frame.pack(side='top', pady=5)
         cancel_button = ttk.Button(
             master=self._frame, text='cancel', command=self._cancel_command)
         cancel_button.pack()

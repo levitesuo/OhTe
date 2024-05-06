f92de7f124d8b8a79fe407c850b4518025f34a58
levitesuo <leevi.suotula@gmail.com>
2024-05-06 10:12:09 +0300

    Added docstring to repositories

--- projekti/src/repositories/grid_repository.py
+++ projekti/src/repositories/grid_repository.py
@@ -4,10 +4,54 @@ from database_connection import get_database_connection, get_test_connection
 
 
 class GridRepository:
+    '''
+    A class to enable the saving and fetching of grids from the database.
+
+    ...
+
+    Attributes
+    ----------
+    _connection : Connection
+        a connection to the database
+
+    Methods
+    -------
+    save_grid(grid: Board, user_id)
+        Saves a grid to the database with the given user_id.
+
+    get_grid_by_id(grid_id)
+        Fetches a grid from the database by its id.
+
+    get_grid_by_name(grid_name)
+        Fetches a grid from the database by its name.
+
+    get_all()
+        Fetches all grids from the database.
+    '''
+
     def __init__(self, connection):
+        '''
+        Constructs all the necessary attributes for the grid repository object.
+
+        Parameters
+        ----------
+        connection : Connection
+            a connection to the database
+        '''
         self._connection = connection
 
     def save_grid(self, grid: Board, user_id):
+        '''
+        Saves a grid to the database with the given user_id.
+
+        Parameters
+        ----------
+        grid : Board
+            the grid to be saved
+
+        user_id : int
+            the id of the user who is saving the grid
+        '''
         cursor = self._connection.cursor()
         grid = grid.get_grid()
 
@@ -20,6 +64,19 @@ class GridRepository:
         self._connection.commit()
 
     def get_grid_by_id(self, grid_id):
+        '''
+        Fetches a grid from the database by its id.
+
+        Parameters
+        ----------
+        grid_id : int
+            the id of the grid to be fetched
+
+        Returns
+        -------
+        Board
+            the grid fetched from the database as a board object
+        '''
         cursor = self._connection.cursor()
 
         cursor.execute("SELECT * FROM grids WHERE grids.grid_id = ?",
@@ -34,6 +91,19 @@ class GridRepository:
         return new_board
 
     def get_grid_by_name(self, grid_name):
+        '''
+        Fetches a grid from the database by its name.
+
+        Parameters
+        ----------
+        grid_name : str
+            the name of the grid to be fetched
+
+        Returns
+        -------
+        Board
+            the grid fetched from the database as a board object
+        '''
         cursor = self._connection.cursor()
 
         cursor.execute("SELECT * FROM grids WHERE grids.name = ?",
@@ -48,6 +118,14 @@ class GridRepository:
         return new_board
 
     def get_all(self):
+        '''
+        Fetches all grids from the database.
+
+        Returns
+        -------
+        list
+            a list of all grids fetched from the database as board objects
+        '''
         cursor = self._connection.cursor()
 
         cursor.execute("SELECT * FROM grids;")
--- projekti/src/repositories/user_repository.py
+++ projekti/src/repositories/user_repository.py
@@ -3,10 +3,36 @@ from database_connection import get_database_connection, get_test_connection
 
 
 class UserRepository:
+    '''
+    A class to enable the saving and fetching of users from the database.
+
+    ...
+    Attributes
+    ----------
+    _connection : Connection
+        a connection to the database
+
+    Methods
+    -------
+    register_user(user: User)
+        Saves a user to the database.
+
+    find_by_username(username)
+        Fetches a user from the database by its username. Checks if the user exists.
+    '''
+
     def __init__(self, connection):
         self._connection = connection
 
     def register_user(self, user: User):
+        '''
+        Saves a user to the database.
+
+        Parameters
+        ----------
+        user : User
+            the user to be saved as a User object
+        '''
         cursor = self._connection.cursor()
 
         cursor.execute("INSERT INTO users (user_id, username, password) values (?, ?, ?)",
@@ -15,6 +41,19 @@ class UserRepository:
         self._connection.commit()
 
     def find_by_username(self, username):
+        '''
+        Fetches a user from the database by its username. Checks if the user exists.
+
+        Parameters
+        ----------
+        username : str
+            the username of the user to be fetched
+
+        Returns
+        -------
+        User
+            the user object fetched from the database as User object. If the user does not exist, returns None.
+        '''
         cursor = self._connection.cursor()
 
         cursor.execute("SELECT * FROM users WHERE users.username = ?",

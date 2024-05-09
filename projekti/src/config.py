ff093d7faac9a6d784e0ff41fc2710a349f0467f
levitesuo <leevi.suotula@gmail.com>
2024-05-09 19:39:13 +0300

    Added dotenv support

--- projekti/src/config.py
+++ projekti/src/config.py
@@ -1,8 +1,15 @@
 import os
+from dotenv import load_dotenv
 dirname = os.path.dirname(__file__)
 
-DATABASE_FILENAME = "ohte_gol.sqlite"
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+except FileNotFoundError:
+    pass
+
+DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "ohte_gol.sqlite"
 DATABASE_FILE_PATH = os.path.join(dirname, "..", DATABASE_FILENAME)
 
-TEST_DATABASE_FILENAME = "test_ohte_gol.sqlite"
+TEST_DATABASE_FILENAME = os.getenv(
+    "TEST_DATABASE_FILENAME") or "ohte_gol_test.sqlite"
 TEST_DATABASE_FILE_PATH = os.path.join(dirname, "..", DATABASE_FILENAME)
--- projekti/src/database_connection.py
+++ projekti/src/database_connection.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 from config import DATABASE_FILE_PATH, TEST_DATABASE_FILE_PATH
 
 connection = sqlite3.connect(DATABASE_FILE_PATH)
